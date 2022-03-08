b6a086cd78c7b5e00751ae70a760e72268fa1bbe
littlebigmats <matias.sugranes@gmail.com>
2022-03-08 14:09:56 -0600

    projeto mvc

---
+++ Interface/IEquipe.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication6.Models;
+using System.Collections.Generic;
+namespace WebApplication6.Interface
+{
+    public interface IEquipe
+    {
+        //CONTRATO.
+        //Representa os métodos que são obrigatorios em uma classe.
+
+        void Criar(Equipe novaequipe);
+        List<Equipe> LerEquipes();
+    }
+}
